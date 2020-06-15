4dde231221d1f2964aa73153550802cc99c014db
MBailor <65557271+MBailor@users.noreply.github.com>
2020-06-15 12:35:52 -0700

    pushing JS 9 for review

--- JS/example.js
+++ JS/example.js
@@ -161,4 +161,4 @@ function slice_Method() {
   var Sentence = "All work and no play makes Jack a dull boy.";
   var Section = Sentence.slice(27,33);
   document.getElementById("Slice").innerHTML = Section;
-
+}
